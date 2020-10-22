b6b340a9daccb1ef4e370ddd8078cefcd4a43f46
Nate Robinson <n8rrobinson@gmail.com>
2020-10-22 01:32:04 -0600

    fixed installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,12 @@
 echo "installing packages"
 pip -r requirements.txt
 #pip3 -r requirements.txt
-echo "unzip transformers directory"
+echo "unzip transformers directory and corpus"
 unzip transformers.zip
+unzip small_corpus.zip
 mv transformers/run_generation.py .
+echo "make necessary directories"
 mkdir data
 mkdir classifiers
 mkdir npi_models
+echo "done"
